65c86ac0b68fc99d3d5867f119b19b2264a14835
namhd94 <namhd94@gmail.com>
2020-02-08 11:51:29 +0700

    Install angular 9 Redesign feed structure

--- src/app/model/feed.ts
+++ src/app/model/feed.ts
@@ -1,8 +1,25 @@
-import { FeedInfo } from './feed-info';
-import { FeedEntry } from './feed-entry';
-
 export interface Feed {
     status: string;
     feed: FeedInfo;
     items: FeedEntry[];
+}
+
+interface FeedInfo {
+    url: string;
+    title: string;
+    link: string;
+    author: string;
+    description: string;
+    image: string;
+}
+
+interface FeedEntry {
+    title: string;
+    pubDate: string;
+    link: string;
+    guid: string;
+    author: string;
+    thumbnail: string;
+    description: string;
+    content: string;
 }
